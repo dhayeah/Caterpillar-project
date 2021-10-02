9df06eafbc4de8258c0cfa04d718e352cec0c25c
dhayeah <57786651+dhayeah@users.noreply.github.com>
2021-10-02 22:16:54 +0530

    Sqlite added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { Downloader} from '@ionic-native/downloader/ngx';
-
+import { Zip } from '@ionic-native/zip/ngx';
+import {File} from '@ionic-native/file/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import {Filesystem } from '@capacitor/filesystem';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+import {SQLite,SQLiteObject} from '@ionic-native/sqlite/ngx'
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },Downloader],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },Downloader,Zip,File,SQLite],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
